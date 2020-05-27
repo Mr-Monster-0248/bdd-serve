9193ae5a96d95637bb45165c9122fd2d8ad4fa32
Thibault LEPEZ <thibault.lepez@efrei.net>
2020-05-27 23:41:13 +0200

    Add add and delete for patients

--- routes/patient.js
+++ routes/patient.js
@@ -1,20 +1,81 @@
 const express = require('express');
 const router = express.Router();
+const client = require('../config/db');
 
-router.get('/', (req, res) => {
-    // TODO: get all patient
-    res.status(501).send();
+const { isAdmin } = require('../validations/signedIn');
+const { patientValidation, patientIDValidation } = require('../validations/patientValidation');
+
+router.get('/', isAdmin, (req, res) => {
+  const query = {
+    text: 'SELECT patient_id, last_name, first_name FROM patient'
+  };
+
+  client.query(query)
+    .then(dbRes => {
+      res.send(dbRes.rows)
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    })
 });
 
-router.get('/:id', (req, res) => {
-    // TODO: get specific patient
-    res.status(501).send();
+router.get('/:id', isAdmin, (req, res) => {
+  const { error } = patientIDValidation(req.params.id);
+  if (error) return res.status(400).send(error);
+
+  const query = {
+    text: 'SELECT patient_id, last_name, first_name FROM patient WHERE (patient_id = $1)',
+    values: [req.params.id]
+  };
+
+  client.query(query)
+    .then(dbRes => {
+      if (dbRes.rowCount === 0) return res.status(400).send('Wrong id');
+      res.send(dbRes.rows)
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    })
 });
 
-router.post('/add', (req, res) => {
-    // TODO: add patient
-    // TODO: check input
-    res.status(501).send();
+router.post('/add', isAdmin, (req, res) => {
+  const { error } = patientValidation(req.body);
+  if (error) return res.status(400).send(error);
+
+  const query = {
+    text: `INSERT INTO patient
+      (
+        last_name,
+        first_name,
+        date_of_birth,
+        address,
+        email,
+        password,
+        gender,
+        age_category,
+        discovery_id_fk
+      ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`,
+    values: [
+      req.body.last_name,
+      req.body.first_name,
+      req.body.date_of_birth,
+      req.body.address,
+      req.body.email,
+      req.body.password,
+      req.body.gender,
+      req.body.age_category,
+      req.body.discovery_id_fk,
+    ]
+  }
+
+  client.query(query)
+    .then(() => {
+      res.send(true)
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send(false);
+    });
 });
 
 router.put('/:id', (req, res) => {
@@ -22,9 +83,23 @@ router.put('/:id', (req, res) => {
     res.status(501).send();
 })
 
-router.delete('/:id', (req, res) => {
-    // TODO: delete patient
-    res.status(501).send()
+router.delete('/:id', isAdmin, (req, res) => {
+  const { error } = patientIDValidation(req.params.id);
+  if (error) return res.status(400).send(error);
+
+  const query = {
+    text: 'DELETE FROM patient WHERE (patient_id = $1)',
+    values: [req.params.id]
+  };
+
+  client.query(query)
+    .then(dbRes => {
+      if (dbRes.rowCount === 0) return res.status(400).send(false);
+      res.send(true);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    })
 })
 
 module.exports = router;
---
+++ validations/patientValidation.js
@@ -0,0 +1,29 @@
+const Joi = require('@hapi/joi');
+
+module.exports.patientIDValidation = (data) => {
+  const patientValid = Joi.number().min(1).required();
+
+  const { error } = patientValid.validate(data);
+
+  if (error) return { error: error.details[0].message };
+  else return { error: false };
+}
+
+module.exports.patientValidation = (data) => {
+  const patientValid = new Joi.object({
+    last_name: Joi.string().required(),
+    first_name: Joi.string().required(),
+    date_of_birth: Joi.string().required(),
+    address: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+    gender: Joi.number().min(0).max(2),
+    age_category: Joi.number().min(0).max(4),
+    discovery_id_fk: Joi.number().min(1).max(4),
+  });
+
+  const { error } = patientValid.validate(data);
+
+  if (error) return { error: error.details[0].message };
+  else return { error: false };
+}
