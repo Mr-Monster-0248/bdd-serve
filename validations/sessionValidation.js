6f6032d59597e93763a7930f8d2fb0cf5567aea1
Thibault LEPEZ <thibault.lepez@efrei.net>
2020-05-29 00:33:05 +0200

    Add validation for id and update

--- validations/sessionValidation.js
+++ validations/sessionValidation.js
@@ -1,10 +1,34 @@
 const Joi = require('@hapi/joi');
 
+module.exports.sessionIDValidation = (data) => {
+  const sessionValid = Joi.number().min(1).required();
+
+  const { error } = sessionValid.validate(data);
+
+  if (error) return { error: error.details[0].message };
+  else return { error: false };
+}
+
 module.exports.sessionValidation = (data) => {
   const sessionValid = new Joi.object({
     session_group: Joi.array().required(),
     date_time: Joi.string().required(),
     duration: Joi.string().required(),
+  });
+
+  const { error } = sessionValid.validate(data);
+
+  if (error) return { error: error.details[0].message };
+  else return { error: false };
+}
+
+module.exports.sessionUpdateValidation = (data) => {
+  const sessionValid = new Joi.object({
+    session_group: Joi.array().required(),
+    date_time: Joi.string().required(),
+    duration: Joi.string().required(),
+    mode: Joi.number().min(1).max(3).required(),
+    paid: Joi.boolean().required(),
   });
 
   const { error } = sessionValid.validate(data);
