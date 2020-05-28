38d32687edaac97f2f67d435c8121090021eb910
Thibault LEPEZ <thibault.lepez@efrei.net>
2020-05-28 23:10:56 +0200

    Add get for sessions

--- routes/session.js
+++ routes/session.js
@@ -5,12 +5,95 @@ const { isSignedIn, isAdmin } = require('../validations/signedIn');
 const { sessionValidation } = require('../validations/sessionValidation');
 
 router.get('/', isAdmin, (req, res) => {
-    res.status(501).send();
+  const query = {
+    text: `SELECT
+              session.session_id,
+              session_group.participant_1_id_fk   AS Part_1,
+              session_group.participant_2_id_fk   AS Part_2,
+              session_group.participant_3_id_fk   AS Part_3,
+              session.date_and_time,
+              session.duration,
+              payment.price,
+              means_of_payment.mode,
+              payment.paid
+            FROM session
+            INNER JOIN session_group ON session.group_id_fk = session_group.group_id
+            INNER JOIN payment ON payment.payment_id = session.payment_id_fk
+            INNER JOIN means_of_payment ON means_of_payment.payment_mode_id = payment.payment_mode_id`,
+  }
+
+  client.query(query)
+    .then(dbRes => {
+      const formatedData = dbRes.rows.map(el => {
+        return {
+          session_id: el.session_id,
+          patients: [
+            el.part_1,
+            el.part_2,
+            el.part_3,
+          ],
+          date_and_time: el.date_and_time,
+          duration: el.duration,
+          price: el.price,
+          mode: el.mode,
+          paid: el.paid,
+        }
+
+      })
+      res.send(formatedData);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send(err);
+    });
+
 });
 
 router.get('/:id', (req, res) => {
-    // TODO: aget specific events
-    res.status(501).send();
+    const query = {
+    text: `SELECT
+              session.session_id,
+              session_group.participant_1_id_fk   AS Part_1,
+              session_group.participant_2_id_fk   AS Part_2,
+              session_group.participant_3_id_fk   AS Part_3,
+              session.date_and_time,
+              session.duration,
+              payment.price,
+              means_of_payment.mode,
+              payment.paid
+            FROM session
+            INNER JOIN session_group ON session.group_id_fk = session_group.group_id
+            INNER JOIN payment ON payment.payment_id = session.payment_id_fk
+            INNER JOIN means_of_payment ON means_of_payment.payment_mode_id = payment.payment_mode_id
+            WHERE session.session_id = $1`,
+    values: [req.params.id]
+  }
+
+  client.query(query)
+    .then(dbRes => {
+      if (dbRes.rowCount === 0) return res.status(400).send('Wrong id');
+      const formatedData = dbRes.rows.map(el => {
+        return {
+          session_id: el.session_id,
+          patients: [
+            el.part_1,
+            el.part_2,
+            el.part_3,
+          ],
+          date_and_time: el.date_and_time,
+          duration: el.duration,
+          price: el.price,
+          mode: el.mode,
+          paid: el.paid,
+        }
+
+      })
+      res.send(formatedData);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send(err);
+    });
 });
 
 router.post('/add', isAdmin, async (req, res) => {
@@ -20,7 +103,7 @@ router.post('/add', isAdmin, async (req, res) => {
   const price = 49.99 * req.body.session_group.length;
   const paymentQuery = {
     text: `INSERT INTO payment (price, payment_mode_id, paid)
-            VALUES ($1 , null, false)
+            VALUES ($1 , 3, false)
             RETURNING payment_id;`,
     values: [price],
   }
@@ -56,8 +139,8 @@ router.post('/add', isAdmin, async (req, res) => {
       values: [
         req.body.date_time,
         req.body.duration,
-        payment_id,
         group_id,
+        payment_id,
       ]
     });
 
