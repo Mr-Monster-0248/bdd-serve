02d7e2ffb78e433c901a0a63db15732bb8e6d42c
Thibault LEPEZ <thibault.lepez@efrei.net>
2020-05-29 00:34:04 +0200

    Improve add session and add remove and update

--- routes/session.js
+++ routes/session.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const client = require('../config/db');
 const { isSignedIn, isAdmin } = require('../validations/signedIn');
-const { sessionValidation } = require('../validations/sessionValidation');
+const { sessionValidation, sessionIDValidation, sessionUpdateValidation } = require('../validations/sessionValidation');
 
 router.get('/', isAdmin, (req, res) => {
   const query = {
@@ -49,8 +49,11 @@ router.get('/', isAdmin, (req, res) => {
 
 });
 
-router.get('/:id', (req, res) => {
-    const query = {
+router.get('/:id', isSignedIn, (req, res) => {
+  const { error } = sessionIDValidation(req.params.id);
+  if (error) return res.status(400).send(error);
+
+  const query = {
     text: `SELECT
               session.session_id,
               session_group.participant_1_id_fk   AS Part_1,
@@ -96,73 +99,74 @@ router.get('/:id', (req, res) => {
     });
 });
 
-router.post('/add', isAdmin, async (req, res) => {
+router.post('/add', isAdmin, (req, res) => {
   const { error } = sessionValidation(req.body);
   if (error) return res.status(400).send(error);
 
-  const price = 49.99 * req.body.session_group.length;
-  const paymentQuery = {
-    text: `INSERT INTO payment (price, payment_mode_id, paid)
-            VALUES ($1 , 3, false)
-            RETURNING payment_id;`,
-    values: [price],
+  const query = {
+    text: 'CALL add_session($1, $2, $3, $4, $5, $6)',
+    values: [
+      req.body.session_group[0],
+      req.body.session_group[1],
+      req.body.session_group[2],
+      req.body.session_group.length * 49.99,
+      req.body.date_time,
+      req.body.duration,
+    ]
   }
 
-  const groupQuery = {
-    text: `INSERT INTO session_group
-              (
-                participant_1_id_fk,
-                participant_2_id_fk,
-                participant_3_id_fk
-              )
-            VALUES ($1, $2, $3)
-            RETURNING group_id;`,
-    values: req.body.session_group,
-  }
-
-  try {
-    let dbRes = await client.query(paymentQuery);
-    const payment_id = dbRes.rows[0].payment_id;
-
-    dbRes = await client.query(groupQuery);
-    const group_id = dbRes.rows[0].group_id;
-
-    await client.query({
-      text: `INSERT INTO session
-                (
-                  date_and_time,
-                  duration,
-                  group_id_fk,
-                  payment_id_fk
-                )
-              VALUES ($1, $2, $3, $4);`,
-      values: [
-        req.body.date_time,
-        req.body.duration,
-        group_id,
-        payment_id,
-      ]
+  client.query(query)
+    .then(() => {
+      res.send(true);
+    })
+    .catch(err => {
+      res.status(500).send(err);
     });
-
-    res.send(true);
-  } catch(err) {
-    console.error(err);
-    res.status(500).send(err)
-  }
-
-  // TODO: add event
-  // TODO: check input
-  res.status(501).send();
 });
 
-router.put('/:id', (req, res) => {
-    // TODO: update event
-    res.status(501).send();
+router.patch('/:id', (req, res) => {
+  const { error } = sessionUpdateValidation(req.body);
+  if (error) return res.status(400).send(error);
+
+  const { idErr } = sessionIDValidation(req.body);
+  if (idErr) return res.status(400).send(idErr);
+
+  const query = {
+    text: 'CALL update_session ($1, $2, $3, $4, $5, $6, $7, $8)',
+    values: [
+      req.params.id,
+      req.body.session_group[0],
+      req.body.session_group[1],
+      req.body.session_group[2],
+      req.body.session_group.length * 49.99,
+      req.body.mode,
+      req.body.paid,
+      req.body.date_time,
+    ]
+  }
+
+  client.query(query)
+      .then((dbRes) => {
+        res.send(dbRes);
+      })
+      .catch(err => {
+        res.status(500).send(err);
+      });
 })
 
-router.delete('/:id', (req, res) => {
-    // TODO: delete event
-    res.status(501).send()
+router.delete('/:id', isAdmin, (req, res) => {
+    const query = {
+      text: 'CALL remove_session($1)',
+      values: [req.params.id]
+    }
+
+    client.query(query)
+      .then(() => {
+        res.send(true);
+      })
+      .catch(err => {
+        res.status(500).send(err);
+      });
 })
 
 module.exports = router;
